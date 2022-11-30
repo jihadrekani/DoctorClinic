1b363586efbe03f839d56e77d47edd78d8f99853
jihadrekani <jihadfed95@gmail.com>
2022-11-30 21:52:12 +0300

    init navbar Components

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbars from './Components/Nav/Navbar';
+
+function App() {
+  return (
+    <div >
+    <Navbars />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Nav/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
+import './Nav.css'; 
+import logo from '../../assets/logo.png'
+export default function Navbars() {
+  return (
+    <Navbar expand="lg">
+    <Container>
+      <Navbar.Brand href="#home" >
+        <img src={logo} title="logo"/>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link className='active' href="#home">Home</Nav.Link>
+          <NavDropdown title="Pages" id="basic-nav-dropdown">
+            <NavDropdown.Item href='#action3.1'>About Us</NavDropdown.Item>
+            <NavDropdown.Item href='#action3.1'>Our Team</NavDropdown.Item>
+            <NavDropdown.Item href='#action3.1'>FAQ's</NavDropdown.Item>
+            <NavDropdown.Item href='#action3.1'>Booking</NavDropdown.Item>
+            <NavDropdown.Item href='#action3.1'>Error 404</NavDropdown.Item>
+            <NavDropdown.Item href='#action3.1'>Login / Register</NavDropdown.Item>
+          </NavDropdown>
+
+          <NavDropdown title="Services" id="basic-nav-dropdown">
+          <NavDropdown.Item href='#action3.1'>Servises</NavDropdown.Item>
+          <NavDropdown.Item href='#action3.1'>Servises Details</NavDropdown.Item>
+
+          </NavDropdown>
+
+          <NavDropdown  title="Blog" id="basic-nav-dropdown">
+          <NavDropdown.Item href='#action3.1'>Blogs</NavDropdown.Item>
+          <NavDropdown.Item href='#action3.1'>Blogs Details</NavDropdown.Item>
+
+          </NavDropdown>
+          <Nav.Link href="#home">Contact us</Nav.Link>
+          <Nav.Link href="#home">Search</Nav.Link>
+          <Nav.Link href="#home">Icon + (+964)7932888</Nav.Link>
+          <Nav.Link>
+            <button>Contact Us <span> > </span></button>
+          </Nav.Link>
+
+
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+}
