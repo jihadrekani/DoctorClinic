35926117a951861923240b4d82cccbd9f55b0eca
jihadrekani <jihadfed95@gmail.com>
2022-12-07 20:23:22 +0300

    init reacr font awesome

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquare } from '@fortawesome/free-solid-svg-icons'
 import headergirl from '../assets/headergirl.png';
 import './Home.css';
 const Home = ()=> {
@@ -10,9 +12,14 @@ const Home = ()=> {
                   <h5>We Provide All Health Care Solution</h5>
                   <h2>Protect Your Health And Take Care To Of Your Health</h2>
                   <button><a href='' >Read More</a></button> 
+                  <span>+</span>
                 </div>
                 <div className='col-lg-6 col-md-6'>
-                <img src={headergirl}/>
+              <div className='header-box'>
+              <img src={headergirl}/>
+                {/* installing FontAwesome second video min 23:00 */}
+                <FontAwesomeIcon icon={faSquare}/>
+              </div>
                 </div>
             </div>
         </div>
