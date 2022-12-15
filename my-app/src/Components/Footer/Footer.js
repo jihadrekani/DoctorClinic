809e907e7654225589dd4a008a2fa2380f2d12ce
jihadrekani <jihadfed95@gmail.com>
2022-12-15 22:18:05 +0300

    init Footer Component

--- my-app/src/Components/Footer/Footer.js
+++ my-app/src/Components/Footer/Footer.js
@@ -1,82 +1,117 @@
 import React from "react";
 import logo from '../../assets/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faEnvelope, faPhone, faTwitter} from '@fortawesome/free-solid-svg-icons'
 import facebook from '../../assets/facebook.png'
 import twitter from '../../assets/twitter.png'
 import instagram from '../../assets/instagram.png'
 import linkedin from '../../assets/linkedin.png'
+
 import './Footer.css'
 
-const Footer = () => {
-  return (
-    <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3 col-sm-6">
-              <img src={logo} className='footer-logo'/>
-              <p>Lorem ipsum is dolor sit amet, csectetur adipiscing elit, dolore smod tempor incididunt ut labore et.</p>
-              <div className="footer-contact">
-                <div className="footer-icon">
-                   <FontAwesomeIcon icon={faPhone}/>
-                </div>
-                <div className="footer-text">
-                    <h6>Contact Us</h6>
-                    <h4>(+964)7507932888</h4>
-                </div>
-              </div>
-          </div>
-          <div className="col-md-3 col-sm-6">
-              <h2>Quick Links</h2>
-              <ul>
-                <li><a href="" >Home</a></li>
-                <li><a href="" >About</a></li>
-                <li><a href="" >Blog</a></li>
-                <li><a href="" >Booking</a></li>
-                <li><a href="" >FAQs</a></li>
-                <li><a href="" >Our Team</a></li>
-                <li><a href="" >Services</a></li>
-              </ul>
-          </div>
-          <div className="col-md-3 col-sm-6">
-          <h2>Our Services</h2>
-              <ul>
-                <li><a href="" >Dental Care</a></li>
-                <li><a href="" >Cardiac Clinic</a></li>
-                <li><a href="" >Massege Therapy</a></li>
-                <li><a href="" >Cardiology</a></li>
-                <li><a href="" >Precise Diagnosis</a></li>
-                <li><a href="" >Abmbulance Services</a></li>
-                <li><a href="" >Services</a></li>
-              </ul>
-          </div>
-          <div className="col-md-3 col-sm-6">
-          <h2>Subcribe</h2>
-          <form>
-            <input type="email" placeholder="Enter Your Email"/>
-            <button type="submit">Subscribe Now</button>
-          </form>
-          <ul className="social">
-            <li><a href="#"><img src={facebook}/></a></li>
-            <li><a href="#"><img src={twitter}/></a></li>
-            <li><a href="#"><img src={instagram}/></a></li>
-            <li><a href="#"><img src={linkedin}/></a></li>
-          </ul>
-               
-          </div>
+const Footer = () =>{
+  return(
+    // <!-- Footer -->
+<footer className="text-center text-lg-start bg-white text-muted">
+  {/* <!-- Section: Social media --> */}
+  <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    {/* <!-- Left --> */}
+   
+    {/* <!-- Right --> */}
+  </section>
+  {/* <!-- Section: Social media --> */}
+
+  {/* <!-- Section: Links  --> */}
+  <section className="">
+    <div className="container text-center text-md-start mt-5">
+      {/* <!-- Grid row --> */}
+      <div className="row mt-3">
+        {/* <!-- Grid column --> */}
+        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          {/* <!-- Content --> */}
+          <h6 className="text-uppercase fw-bold mb-4">
+            <i className="fas fa-gem me-3 text-secondary"></i>
+            <img src={logo}/>
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
         </div>
-        <div className="footer-bottom">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12 col-md-12">
-                        <p>Copyright © 2022 Design & Developed by <span>Jihad Issa</span></p>
-                    </div>
-                </div>
-            </div>
+        {/* <!-- Grid column --> */}
+
+        {/* <!-- Grid column --> */}
+        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          {/* <!-- Links --> */}
+          <h6 className="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" className="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset">Laravel</a>
+          </p>
         </div>
+        {/* <!-- Grid column --> */}
+
+        {/* <!-- Grid column -->/ */}
+        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          {/* <!-- Links --> */}
+          <h6 className="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" className="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset">Help</a>
+          </p>
+        </div>
+        {/* <!-- Grid column --> */}
+
+        {/* <!-- Grid column -->/ */}
+        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          {/* <!-- Links --> */}
+          <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><FontAwesomeIcon icon={faHome} className="home"/> Duhok, Barzan Street, Kurdistan</p>
+          
+          <p><FontAwesomeIcon icon={faEnvelope} className="envelope me-2"/>
+            info@example.com  
+          </p>
+          <p><FontAwesomeIcon icon={faPhone} className="phone"/> + 01 234 567 88</p>
+          <p><FontAwesomeIcon icon={faPhone} className="phone"/> + 01 234 567 89</p>
+        </div>
+        {/* <!-- Grid column --> */}
       </div>
-    </footer>
-  );
-};
+      {/* <!-- Grid row --> */}
+    </div>
+  </section>
+  {/* <!-- Section: Links  --> */}
+
+  {/* <!-- Copyright --> */}
+  <div className="text-center p-4">
+    © 2023 Copyright:
+    <a className="text-reset jihad fw-bold" href="https://jihadissa.netlify.app/">jihad Issa</a>
+  </div>
+  {/* <!-- Copyright --> */}
+</footer>
+// <!-- Footer -->
+  )
+}
+
+
 
 export default Footer;
