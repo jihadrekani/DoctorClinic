a9399e7c2b20535dc423fc81649cffca2163cf6b
jihadrekani <jihadfed95@gmail.com>
2022-12-23 17:30:06 +0300

    completed News and NewsHeader comp.

--- my-app/src/Components/Footer/Footer.js
+++ my-app/src/Components/Footer/Footer.js
@@ -13,37 +13,16 @@ const Footer = () =>{
   return(
     // <!-- Footer -->
 <footer className="text-center text-lg-start bg-white text-muted">
-  {/* <!-- Section: Social media --> */}
-  <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-    {/* <!-- Left --> */}
-   
-    {/* <!-- Right --> */}
-  </section>
-  {/* <!-- Section: Social media --> */}
 
-  {/* <!-- Section: Links  --> */}
   <section className="">
     <div className="container text-center text-md-start mt-5">
       {/* <!-- Grid row --> */}
       <div className="row mt-3">
+        
         {/* <!-- Grid column --> */}
-        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          {/* <!-- Content --> */}
-          <h6 className="text-uppercase fw-bold mb-4">
-            <i className="fas fa-gem me-3 text-secondary"></i>
-            <img src={logo}/>
-          </h6>
-          <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
-          </p>
-        </div>
-        {/* <!-- Grid column --> */}
-
-        {/* <!-- Grid column --> */}
-        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        <div className=" col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           {/* <!-- Links --> */}
-          <h6 className="text-uppercase fw-bold mb-4">
+          <h6 className="text-uppercase  fw-bold mb-4">
             Products
           </h6>
           <p>
@@ -62,10 +41,10 @@ const Footer = () =>{
         {/* <!-- Grid column --> */}
 
         {/* <!-- Grid column -->/ */}
-        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+        <div className="col-md-1 col-lg-2 col-xl-2 mx-auto mb-4">
           {/* <!-- Links --> */}
           <h6 className="text-uppercase fw-bold mb-4">
-            Useful links
+             links
           </h6>
           <p>
             <a href="#!" className="text-reset">Pricing</a>
@@ -86,7 +65,7 @@ const Footer = () =>{
         <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           {/* <!-- Links --> */}
           <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><FontAwesomeIcon icon={faHome} className="home"/> Duhok, Barzan Street, Kurdistan</p>
+          <p><FontAwesomeIcon icon={faHome} className="home"/> Duhok, Barzan Street </p>
           
           <p><FontAwesomeIcon icon={faEnvelope} className="envelope me-2"/>
             info@example.com  
@@ -102,7 +81,7 @@ const Footer = () =>{
   {/* <!-- Section: Links  --> */}
 
   {/* <!-- Copyright --> */}
-  <div className="text-center p-4">
+  <div className="text-center copyfoot p-4">
     © 2023 Copyright:
     <a className="text-reset jihad fw-bold" href="https://jihadissa.netlify.app/">jihad Issa</a>
   </div>
